c81ed0e38bee9bdb83ea1ade1b4beb9375c6b482
Kevin <kevinvosburgh4@gmail.com>
2020-12-19 16:25:50 -0500

    Optomizations to speed up auction intake

--- Database/auction.py
+++ Database/auction.py
@@ -5,14 +5,31 @@ from DBModels.Auction import Auction
 from Database.item import addItemById, get_item_by_name
 from Models.auction import GetAuction
 
+
 def auction_exists(auctionId):
-    if session.query(Auction).filter(Auction.auctionId == auctionId).first() is not None:
-        return True
-    return False
+    return session.execute('select auctionId from auctions where auctionId = :val',
+                           {'val': auctionId}).first() is not None
+
+
+def insert_or_update_auc(auction):
+    session.execute('''
+    INSERT INTO auctions (auctionId, itemId, quantity, buyout, unitPrice, bid, dateInserted, dirty)
+    VALUES (:id, :itemId, :quantity, :buyout, :unitPrice, :bid, :dateInserted, :dirty)
+    ON DUPLICATE KEY UPDATE
+        quantity = :quantity,
+        bid = :bid,
+        dirty = :dirty
+    ''', {'id': auction.id,
+          'itemId': auction.item.id,
+          'quantity': auction.quantity,
+          'buyout': auction.buyout,
+          'unitPrice': auction.unit_price,
+          'bid': auction.bid,
+          'dateInserted': datetime.datetime.now(),
+          'dirty': 0})
 
 
 def add_all_auctions(auctions: GetAuction):
-    count = 0
     print(len(auctions.auctions))
     auctions.auctions.sort(key=lambda auc: auc.item.id)
     set_auctions_dirty()
@@ -23,9 +40,7 @@ def add_all_auctions(auctions: GetAuction):
         count += 1
         if count % 2000 == 0 and count != 0:
             print("Amount Processed:", count)
-        if auction_exists(auction.id):
-            clean_auction(auction.id, auction.quantity)
-            continue
+
         item_id = auction.item.id
         if item_id != current_item:
             resp = addItemById(item_id)
@@ -33,11 +48,8 @@ def add_all_auctions(auctions: GetAuction):
                 continue
             current_item = item_id
 
-        obj = Auction(auction.id, item_id, auction.quantity, auction.buyout, auction.unit_price,
-                              auction.bid, datetime.datetime.now())
-        session.add(obj)
+        insert_or_update_auc(auction)
         session.commit()
-
 
     remove_dirty_auctions()
     session.commit()
@@ -45,17 +57,18 @@ def add_all_auctions(auctions: GetAuction):
 
 
 def clean_auction(auction_id, new_quantity):
-    session.query(Auction).filter(Auction.auctionId == auction_id).update({ Auction.dirty: 0, Auction.quantity: new_quantity })
+    session.query(Auction).filter(Auction.auctionId == auction_id).update(
+        {Auction.dirty: 0, Auction.quantity: new_quantity})
     session.commit()
 
 
 def set_auctions_dirty():
-    session.query(Auction).filter(Auction.dirty == 0).update({ Auction.dirty: 1 })
+    session.query(Auction).filter(Auction.dirty == 0).update({Auction.dirty: 1})
     session.commit()
 
 
 def remove_dirty_auctions():
-    session.query(Auction).filter(Auction.dirty == 1).update({ Auction.dateRemoved: datetime.datetime.now() })
+    session.query(Auction).filter(Auction.dirty == 1).update({Auction.dateRemoved: datetime.datetime.now()})
     session.commit()
 
 
@@ -63,9 +76,9 @@ def get_auction_data(items, amount_return):
     data = {}
     for item in items:
         itemObj = get_item_by_name(item)
-        auctions = session.query(Auction.unitPrice, Auction.buyout, func.sum(Auction.quantity))\
-            .group_by(Auction.unitPrice, Auction.buyout)\
-            .filter(Auction.itemId == itemObj.itemId, or_(Auction.buyout != None, Auction.unitPrice != None))\
+        auctions = session.query(Auction.unitPrice, Auction.buyout, func.sum(Auction.quantity)) \
+            .group_by(Auction.unitPrice, Auction.buyout) \
+            .filter(Auction.itemId == itemObj.itemId, or_(Auction.buyout != None, Auction.unitPrice != None)) \
             .order_by(Auction.unitPrice.asc()).limit(amount_return).all()
         data[item] = auctions
     return data
@@ -73,4 +86,3 @@ def get_auction_data(items, amount_return):
 
 def get_most_recently_added_date():
     return session.query(Auction.dateInserted).order_by(Auction.dateInserted.desc()).first()
-
